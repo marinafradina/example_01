ba2e0f29c6a16d7b73f848e5d2ba5e2b9df98fbf
Marina <f_marina2003@mail.ru>
2023-11-17 12:08:28 +0100

    add orange object

--- src/Orange.java
+++ src/Orange.java
@@ -2,5 +2,7 @@ public class Orange {
     public static void main(String[] args) {
         Orange orange = new Orange();
         int x = 10;
+        System.out.println();
+        System.out.println(x);
     }
 }
